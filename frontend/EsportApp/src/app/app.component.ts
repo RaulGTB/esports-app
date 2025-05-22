bfa86ba2735486ed52034ec5bdf27e60326fc884
Raul <raulgranadostbgmail.com>
2025-05-22 23:22:28 +0200

    routing y componentes hechos menos favs

--- frontend/EsportApp/src/app/app.component.ts
+++ frontend/EsportApp/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
+import { HomeComponent } from './sections/home/home.component';
 
 @Component({
   selector: 'app-root',
--- frontend/EsportApp/src/app/app.routes.ts
+++ frontend/EsportApp/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './sections/home/home.component';
+import { LoginComponent } from './sections/login/login.component';
+import { RegisterComponent } from './sections/register/register.component';
+import { CategoriesComponent } from './sections/categories/categories.component';
+import { DiscoverComponent } from './sections/discover/discover.component';
+import { FavsComponent } from './sections/favs/favs.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", redirectTo: 'home', pathMatch: 'full'},
+    {path: "home", component: HomeComponent},
+    {path: "categories", component: CategoriesComponent},
+    {path: "discover", component: DiscoverComponent},
+    {path: "favs", component: FavsComponent},
+    {path: "login", component: LoginComponent},
+    {path: "register", component: RegisterComponent},
+    {path: "**", component: HomeComponent}
+];
