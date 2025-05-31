683f94b0376e3d6137a94676eb8ffee93f815f93
RaulGTB <102032194+RaulGTB@users.noreply.github.com>
2025-05-31 09:57:37 +0200

    Delete backend directory

--- backend/rjkscore/src/main/java/rjkscore/Application/Service/AuthService.java
+++
@@ -1,8 +0,0 @@
-package rjkscore.application.service;
-
-import rjkscore.Infrastructure.Dto.Request.RegisterRequestDto;
-import rjkscore.Infrastructure.Dto.Response.AppUserResponseDto;
-
-public interface AuthService {
-    AppUserResponseDto register(RegisterRequestDto dto);
-}
